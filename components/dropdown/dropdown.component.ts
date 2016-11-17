24ffadd6bb39c431d8246a78fcdad053542fc445
Behnam Mazaheri <mazaheri@gmail.com>
2016-11-17 22:49:12 +0330

    dropdown update

--- components/dropdown/dropdown.component.ts
+++ components/dropdown/dropdown.component.ts
@@ -1,9 +1,15 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {ListItem} from "../models/listitem.model";
 
 @Component({
     selector: 'bl-dropdown',
     templateUrl: './dropdown.component.html'
 })
 export class DropdownComponent{
+@Input() items:ListItem[];
+
+constructor(){
+
+}
 
 }
