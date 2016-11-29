b245f6200a85396913a18dafecd631d7f6565575
Behnam Mazaheri <mazaheri@gmail.com>
2016-11-29 11:59:16 +0330

    number of weeks bug resolved

--- components/datepicker/datepicker.component.ts
+++ components/datepicker/datepicker.component.ts
@@ -53,7 +53,7 @@ export class DatepickerComponent {
         let dayNumberofMonthFirst = this._c.dayNumberofMonthFirst(this.currentViewDate);
         this.rows = new Array<Row>();
         let cellIndex = 0;
-        for (let weekIndex = 0; weekIndex < Math.ceil(currentMonthLength / 7); weekIndex++) {
+        for (let weekIndex = 0; weekIndex < Math.ceil((currentMonthLength + dayNumberofMonthFirst) / 7); weekIndex++) {
             this.rows[weekIndex] = new Row();
             for (let colIndex = 0; colIndex < 7; colIndex++) {
                 this.rows[weekIndex].cells[colIndex] = new Cell();
