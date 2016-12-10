73bc53cf6defda83538e0d1674f8e0c85fcd127b
Behnam Mazaheri <mazaheri@gmail.com>
2016-12-10 17:34:11 +0330

    implement milad temporary conversion

--- components/datepicker/calendars/jalali.ts
+++ components/datepicker/calendars/jalali.ts
@@ -10,7 +10,7 @@ export class Jalali implements CalendarBase {
     minYear = 1350;
     maxYear = 1450;
 
-    nameOfMonth(num: number): string{
+    nameOfMonth(num: number): string {
         return this.monthNames[num];
     }
 
@@ -20,9 +20,10 @@ export class Jalali implements CalendarBase {
 
     dateToDateUnit(date: Date): DateUnit {
         let uDate: DateUnit = new DateUnit();
-        uDate.year = date.getFullYear();
-        uDate.month = date.getMonth();
-        uDate.day = date.getDate();
+        let tempDate = this._calCulateDate(date.getFullYear(), date.getMonth(), date.getDate())
+        uDate.year = tempDate.year;
+        uDate.month = tempDate.month;
+        uDate.day = tempDate.day;
         uDate.hour = date.getHours();
         uDate.minute = date.getMinutes();
         uDate.seconds = date.getSeconds();
@@ -50,5 +51,137 @@ export class Jalali implements CalendarBase {
 
     dayNumberOfMonthFirst(date: DateUnit): number {
         return new Date(date.year, date.month, 1).getDay();
+    }
+////////////////////////////////////////////
+    private _miladiLeapYears = [2012, 2016, 2020];
+    private _monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    private leapYearMonthLength = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    private numberOfLeapYears: number;
+    _calculateLeapYears(futureYear: number) {
+        let firstIndex = 0;
+        while (2011 > this._miladiLeapYears[firstIndex]) {
+            firstIndex++;
+        }
+        let secondIndex = 0;
+        while (futureYear > this._miladiLeapYears[secondIndex]) {
+            secondIndex++;
+        }
+        return this.numberOfLeapYears = secondIndex - firstIndex;
+    }
+
+    _calculateDays(year: number, month: number, day: number) {
+        let differenceYear: number;
+        let differenceMonth: number;
+        let d: number;
+        if (month > 2 || (day >= 21 && month == 2)) {
+            this.numberOfLeapYears = this._calculateLeapYears(year + 1);
+            differenceYear = year - 2011;
+            if (month == 2) {
+                d = day - 21;
+            } else {
+                differenceMonth = month - 3;
+                d = 10;
+                for (var i = 0; i < differenceMonth; i++) {
+                    d = d + this._monthLength[i + 3];
+                }
+                d = d + day;
+            }
+            return (365 * differenceYear) + this.numberOfLeapYears + d + 1;
+        } else {
+            this.numberOfLeapYears = this._calculateLeapYears(year);
+            differenceYear = year - 2011 - 1;
+            d = 285;
+            if (year == 2012 || year == 2016 || year == 2020) {
+                for (var i = 0; i < month; i++) {
+                    d = d + this.leapYearMonthLength[i];
+                }
+                d = d + day;
+            } else {
+                for (var i = 0; i < month; i++) {
+                    d = d + this._monthLength[i];
+                }
+                d = d + day;
+            }
+            return (365 * differenceYear) + this.numberOfLeapYears + d + 1;
+        }
+    }
+
+    _calculateMonthAndDay(numberOfDays: number): any {
+        let mah: number;
+        let rooz: number;
+        if (0 <= numberOfDays && numberOfDays <= 186) {
+            if ((numberOfDays) % 31 == 0) {
+                mah = Math.floor((numberOfDays) / 31);
+                rooz = 31;
+                return mah + "/" + rooz;
+            } else {
+                mah = Math.floor((numberOfDays) / 31) + 1;
+                rooz = (numberOfDays) % 31;
+                return mah + "/" + rooz;
+            }
+        } else {
+            numberOfDays = numberOfDays - 186;
+            if ((numberOfDays) % 30 == 0) {
+                mah = Math.floor(numberOfDays / 30) + 6;
+                rooz = 30;
+                return mah + "/" + rooz;
+            }
+            mah = Math.floor(numberOfDays / 30) + 7;
+            rooz = numberOfDays % 30;
+            return { month: mah, day: rooz };
+        }
+    }
+
+    _calCulateDate(year: number, month: number, day: number) {
+        let numberOfDays: number = this._calculateDays(year, month, day);
+        let sal: number;
+        let y: number;
+        if (0 <= numberOfDays && numberOfDays <= 365) {
+            sal = 1390;
+            y = numberOfDays;
+        }
+        else if (365 < numberOfDays && numberOfDays <= 731) {
+            sal = 1391;
+            y = numberOfDays - 365;
+        }
+        else if (731 < numberOfDays && numberOfDays <= 1096) {
+            sal = 1392;
+            y = numberOfDays - 731;
+        }
+        else if (1096 < numberOfDays && numberOfDays <= 1461) {
+            sal = 1393;
+            y = numberOfDays - 1096;
+        }
+        else if (1461 < numberOfDays && numberOfDays <= 1826) {
+            sal = 1394;
+            y = numberOfDays - 1461;
+        }
+        else if (1826 < numberOfDays && numberOfDays <= 2192) {
+            sal = 1395;
+            y = numberOfDays - 1826;
+        }
+        else if (2192 < numberOfDays && numberOfDays <= 2557) {
+            sal = 1396;
+            y = numberOfDays - 2192;
+        }
+        else if (2557 < numberOfDays && numberOfDays <= 2922) {
+            sal = 1397;
+            y = numberOfDays - 2557;
+        }
+        else if (2922 < numberOfDays && numberOfDays <= 3287) {
+            sal = 1398;
+            y = numberOfDays - 2922;
+        }
+        else if (3287 < numberOfDays && numberOfDays <= 3653) {
+            sal = 1399;
+            y = numberOfDays - 3287;
+        }
+        else if (3653 < numberOfDays && numberOfDays <= 4018) {
+            sal = 1400;
+            y = numberOfDays - 3653;
+
+        }
+        let x = this._calculateMonthAndDay(y);
+        return { year: sal, month: x.month, day: x.day }
     }
 }
